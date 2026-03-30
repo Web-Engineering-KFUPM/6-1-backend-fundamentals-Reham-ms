0409fd447e6b9218b8ccd1ee68d20122a00f2cdb
Reham-ms <s20227010@kfupm.edu.sa>
2026-03-30 14:09:21 +0300

    done backend

--- 6-1-back-end-fundamentals/backend/server.js
+++ 6-1-back-end-fundamentals/backend/server.js
@@ -1,25 +1,24 @@
-// TODO 1: Import Express
+const express = require("express");
 
+const app = express();
+const PORT = 3000;
 
+// Root route
+app.get("/", (req, res) => {
+    res.send("Welcome to the Full-Stack Fundamentals Lab!");
+});
 
-// TODO 2: Create the Express app and store it in a variable named app
+// About route
+app.get("/about", (req, res) => {
+    res.send("This is the About page of the Node.js + Express lab.");
+});
 
+// Student route
+app.get("/student", (req, res) => {
+    res.send("Student Name: Reham");
+});
 
-
-// TODO 3: Allow React to access the server
-
-
-
-// TODO 5: Create the home route "/"
-
-
-
-// TODO 6: Create the "/about" route
-
-
-
-// TODO 7: Create the "/student" route
-
-
-
-// TODO 4: Start the server on port 3000
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
